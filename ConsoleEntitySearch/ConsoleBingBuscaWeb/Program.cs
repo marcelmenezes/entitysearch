e34b62dbfc9ba89b3df48768cd9e5ab405078970
28332 <marcel.business@gmail.com>
2019-05-22 11:53:35 -0400

    Entity Search C#

---
+++ ConsoleEntitySearch/ConsoleBingBuscaWeb/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Net;
+using System.IO;
+
+namespace ConsoleBingBuscaWeb
+{
+    class Program
+    {
+        // Enter a valid subscription key.
+        const string accessKey = "e8a0a5c87ce7475480363a5df584c1aa";
+        /*
+         * If you encounter unexpected authorization errors, double-check this value
+         * against the endpoint for your Bing Web search instance in your Azure
+         * dashboard.
+         */
+        const string uriBase = "https://eastus2.api.cognitive.microsoft.com/bing/v7.0/search";
+        //const string searchTerm = "Microsoft Cognitive Services";
+        const string searchTerm = "BANCO BMG S/A- ";
+
+        static void Main(string[] args)
+        {
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            if (accessKey.Length == 32)
+            {
+                Console.WriteLine("Searching the Web for: " + searchTerm);
+                SearchResult result = BingWebSearch(searchTerm);
+                Console.WriteLine("\nRelevant HTTP Headers:\n");
+                foreach (var header in result.relevantHeaders)
+                    Console.WriteLine(header.Key + ": " + header.Value);
+
+                Console.WriteLine("\nJSON Response:\n");
+                Console.WriteLine(JsonPrettyPrint(result.jsonResult));
+            }
+            else
+            {
+                Console.WriteLine("Invalid Bing Search API subscription key!");
+                Console.WriteLine("Please paste yours into the source code.");
+            }
+            Console.Write("\nPress Enter to exit ");
+            Console.ReadLine();
+
+        }
+        struct SearchResult
+        {
+            public String jsonResult;
+            public Dictionary<String, String> relevantHeaders;
+        }
+
+        static SearchResult BingWebSearch(string searchQuery)
+        {
+            // Construct the search request URI.
+            var uriQuery = uriBase + "?q=" + Uri.EscapeDataString(searchQuery);
+
+            // Perform request and get a response.
+            WebRequest request = HttpWebRequest.Create(uriQuery);
+            request.Headers["Ocp-Apim-Subscription-Key"] = accessKey;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponseAsync().Result;
+            string json = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            // Create a result object.
+            var searchResult = new SearchResult()
+            {
+                jsonResult = json,
+                relevantHeaders = new Dictionary<String, String>()
+            };
+
+            // Extract Bing HTTP headers.
+            foreach (String header in response.Headers)
+            {
+                if (header.StartsWith("BingAPIs-") || header.StartsWith("X-MSEdge-"))
+                    searchResult.relevantHeaders[header] = response.Headers[header];
+            }
+            return searchResult;
+        }
+
+        /// <summary>
+        /// Formats the JSON string by adding line breaks and indents.
+        /// </summary>
+        /// <param name="json">The raw JSON string.</param>
+        /// <returns>The formatted JSON string.</returns>
+        static string JsonPrettyPrint(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+                return string.Empty;
+
+            json = json.Replace(Environment.NewLine, "").Replace("\t", "");
+
+            StringBuilder sb = new StringBuilder();
+            bool quote = false;
+            bool ignore = false;
+            char last = ' ';
+            int offset = 0;
+            int indentLength = 2;
+
+            foreach (char ch in json)
+            {
+                switch (ch)
+                {
+                    case '"':
+                        if (!ignore) quote = !quote;
+                        break;
+                    case '\\':
+                        if (quote && last != '\\') ignore = true;
+                        break;
+                }
+
+                if (quote)
+                {
+                    sb.Append(ch);
+                    if (last == '\\' && ignore) ignore = false;
+                }
+                else
+                {
+                    switch (ch)
+                    {
+                        case '{':
+                        case '[':
+                            sb.Append(ch);
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', ++offset * indentLength));
+                            break;
+                        case ']':
+                        case '}':
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', --offset * indentLength));
+                            sb.Append(ch);
+                            break;
+                        case ',':
+                            sb.Append(ch);
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', offset * indentLength));
+                            break;
+                        case ':':
+                            sb.Append(ch);
+                            sb.Append(' ');
+                            break;
+                        default:
+                            if (quote || ch != ' ') sb.Append(ch);
+                            break;
+                    }
+                }
+                last = ch;
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ ConsoleEntitySearch/ConsoleEntitySearch/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Azure.CognitiveServices.Search.WebSearch;
+using Microsoft.Azure.CognitiveServices.Search.WebSearch.Models;
+using Microsoft.Azure.CognitiveServices.Search.EntitySearch;
+using Microsoft.Azure.CognitiveServices.Search.EntitySearch.Models;
+using Newtonsoft.Json;
+
+namespace ConsoleEntitySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                var client = new EntitySearchClient(new Microsoft.Azure.CognitiveServices.Search.EntitySearch.ApiKeyServiceClientCredentials("499a3aed7bd94abbbd063b5386d0057c"));
+                var entityData = client.Entities.Search(query: "Microsoft");
+                var mainEntity = entityData.Entities.Value.Where(thing => thing.EntityPresentationInfo.EntityScenario == EntityScenario.DominantEntity).FirstOrDefault();
+
+
+
+                WebSearchClient clientS = new WebSearchClient(new Microsoft.Azure.CognitiveServices.Search.WebSearch.ApiKeyServiceClientCredentials("e8a0a5c87ce7475480363a5df584c1aa"));
+                clientS.Endpoint = "https://eastus2.api.cognitive.microsoft.com/";
+
+                WebResults(clientS);
+
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+                //var consulta = clientS.Web.SearchAsync(query: "Microsoft");
+
+            }
+        }
+
+        public static async void WebResults(WebSearchClient client)
+        {
+            try
+            {
+                //var webData = await client.Web.SearchAsync(query: "Yosemite National Park");
+                var webData = await client.Web.SearchAsync(query: "BRADESCO");
+                Console.WriteLine("Searching for \"Yosemite National Park\"");
+
+                // Code for handling responses is provided in the next section...
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Encountered exception. " + ex.Message);
+            }
+        }
+    }
+}
